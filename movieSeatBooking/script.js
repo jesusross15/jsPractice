802d8cd17e4ea1ec4451a09fd1d4e542b5a16413
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2024-09-26 21:12:16 -0400

    setting the data to local storage

--- movieSeatBooking/script.js
+++ movieSeatBooking/script.js
@@ -8,13 +8,28 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
 
-console.log(typeof ticketPrice);
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem("selectedMovieIndex", movieIndex);
+    localStorage.setItem("selectedMoviePrice", moviePrice);
+}
 
 // Update selected count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll(".row .seat.selected");
     
-    const selectedSeatsCount = selectedSeats.length
+    // Creating an array of indexes so that way I can save this info to local storage
+    // Copy selected seats into an array with spread operator
+    // Map through that array
+    // Return a new array of indexes
+
+    const seatIndex = [...selectedSeats].map(function(seat) {
+        return [...seats].indexOf(seat);
+    });
+
+    localStorage.setItem("selectedSeats", JSON.stringify(seatIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
 ;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
@@ -22,7 +37,9 @@ function updateSelectedCount() {
 
 // Movie selection event listener
 movieSelect.addEventListener("change", function(e) {
-    ticketPrice = +e.target.value
+    ticketPrice = +e.target.value;
+    // console.log(e.target.selectedIndex, e.target.value);
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
